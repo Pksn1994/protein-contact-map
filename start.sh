662855846caf57f6a784b14f4609c92e36e405ec
Pksn1994 <kurunegaladiba@gmail.com>
2025-06-04 07:14:47 +0000

    linux start script

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "Installing dependencies"
+pip install -r requirements.txt
+
+echo "Navigating to main.py path"
+cd protein_contact_map/contactmap
+
+echo "Running main.py"
+python main.py
+
+
+
+
+# Write-Host "Installing dep"
+# pip install -r requirements.txt
+# Write-Host "set main.py path"
+# Set-Location "protein_contact_map\contactmap"
+# Write-Host "Running main.py"
+# python main.py
